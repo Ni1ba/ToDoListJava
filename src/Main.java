4270863ed9d0ea579c761ab8fc8d0f03bc7b86f4
Ni1ba <danila.baldin@mail.ru>
2024-08-27 13:41:57 +0300

    -добавлены поля (файл и список задач)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    private static final String FILE_NAME = "todolist.txt";
+    private static ArrayList<String> tasks = new ArrayList<>();
+
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
 
     }
+
+
 }
